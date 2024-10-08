23c95d86075b633d50533607d370ac8feeeb0bf0
HimanshuGolyan01 <golyanhimanshu@gmail.com>
2024-10-08 17:26:47 +0530

    Final

--- Backend/index.js
+++ Backend/index.js
@@ -20,11 +20,11 @@ app.use(express.json());
 app.use("/api/v1", rootRouter); 
 
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html")); 
+    res.sendFile(path.resolve(__dirname, "../Frontend", "dist", "index.html")); 
 });
 
 
