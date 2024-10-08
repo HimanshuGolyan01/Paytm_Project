a326af8a7e63a11a1abb4a99ed2dc92541519e57
HimanshuGolyan01 <golyanhimanshu@gmail.com>
2024-10-08 16:13:18 +0530

    Production ready

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,36 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const connectDB = require("./config/db")
-const cors = require("cors")
-const rootRouter = require("./routes/index")
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db"); 
+const cors = require("cors");
+const rootRouter = require("./routes/index"); 
+const path = require("path"); 
+const app = express(); 
 
 
-const app = express()
-dotenv.config()
-connectDB()
-app.use(cors())
-app.use(express.json())
-app.use("/api/v1", rootRouter)
+dotenv.config();
 
-const port = process.env.PORT
+
+connectDB();
+
+
+app.use(cors()); 
+app.use(express.json()); 
+
+
+app.use("/api/v1", rootRouter); 
+
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html")); 
+});
+
+
+const port = process.env.PORT;
+
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`); 
+});
