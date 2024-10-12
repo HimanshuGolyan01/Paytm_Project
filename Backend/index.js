1776b02cc077df225afbb4f90543f106fb59ecf1
HimanshuGolyan01 <golyanhimanshu@gmail.com>
2024-10-12 17:22:05 +0530

    Changes

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db"); 
 const cors = require("cors");
 const rootRouter = require("./routes/index"); 
-const path = require("path"); 
 const app = express(); 
 
 
@@ -19,17 +18,10 @@ app.use(express.json());
 
 app.use("/api/v1", rootRouter); 
 
-const __dirname1 = path.resolve();
-if(process.env.NODE_ENV) {
-    app.use(express.static(path.join(__dirname1, './frontend/dist')));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname1, "./frontend/dist/index.html")); 
-});
-}
 
 
-const port = process.env.PORT;
+
+const port = process.env.PORT || 8000;
 
 
 app.listen(port, () => {
